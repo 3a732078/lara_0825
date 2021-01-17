d546d3f495e7fecee23b1906a29b4ebc6980e0fb
Ben Peng <3a732078@gm.student.ncut.edu.tw>
2021-01-17 13:34:37 +0800

    使用create的方法寫入新的資料，經測試is_featute預設寫入為0

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Models\Post;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Post::create([
+
+    'title'=>"test title ",
+    'content'=>'test content '
+]);
