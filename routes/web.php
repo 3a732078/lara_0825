5878bc8751578e36a71283eed10694db19761b1d
Ben Peng <3a732078@gm.student.ncut.edu.tw>
2021-01-17 14:00:24 +0800

    練習查詢資料

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,14 @@ $p->title = 'test title';
 $p->content = 'test content';
 $p->is_feature = 1;
 $p->save();
+
+$p = Post::all();
+dd($p);
+
+$p = Post::find(1);
+dd($p);
+
+$p = Post::where('id', '<', 10)
+    ->orderBy('id', 'DESC')
+    ->get();
+dd($p);
